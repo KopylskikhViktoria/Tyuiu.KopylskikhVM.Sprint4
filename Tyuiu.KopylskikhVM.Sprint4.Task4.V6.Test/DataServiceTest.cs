826fdc190132502555fec5233ba111d705a0b9de
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-30 22:36:41 +0500

    Task4

---
+++ Tyuiu.KopylskikhVM.Sprint4.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KopylskikhVM.Sprint4.Task4.V6.Lib;
+
+namespace Tyuiu.KopylskikhVM.Sprint4.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] array = new int[,]
+            {
+                {7, 6, 6, 6, 9},
+                {4, 4, 4, 9, 4},
+                {5, 5, 5, 6, 5},
+                {4, 9, 5, 9, 7},
+                {6, 9, 5, 6, 7}
+            };
+
+            int[,] wait = new int[,]
+            {
+                {0, 6, 6, 6, 0},
+                {4, 4, 4, 0, 4},
+                {0, 0, 0, 6, 0},
+                {4, 0, 0, 0, 0},
+                {6, 0, 0, 6, 0}
+            };
+
+            int[,] res = ds.Calculate(array);
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KopylskikhVM.Sprint4.Task4.V6/Program.cs
@@ -0,0 +1,69 @@
+﻿using Tyuiu.KopylskikhVM.Sprint4.Task4.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Копыльских В. М. | НТм-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнила: Копыльских Виктория Максимовна | НТм-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный массив 5x5, заполненный значениями с клавиатуры.          *");
+        Console.WriteLine("* Заменить нечетные элементы на 0.                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] array = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                do
+                {
+                    Console.Write($"Введите элемент [{i},{j}] (от 4 до 9): ");
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                while (array[i, j] < 4 || array[i, j] > 9);
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        int[,] res = ds.Calculate(array);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Измененный массив:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(res[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
