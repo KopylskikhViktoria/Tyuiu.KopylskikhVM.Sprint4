d1a1f4d8b201f94504d27fb832e9f809269367da
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-30 22:50:29 +0500

    Task5

---
+++ Tyuiu.KopylskikhVM.Sprint4.Task5.V29.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KopylskikhVM.Sprint4.Task5.V29.Lib;
+
+namespace Tyuiu.KopylskikhVM.Sprint4.Task5.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] array = new int[,]
+            {
+                { -1, 2, 3, -4, 5 },
+                { 0, -2, 6, 7, -8 },
+                { 9, -1, 2, -3, 4 },
+                { -5, 6, -7, 8, -9 },
+                { 1, -2, 3, -4, 5 }
+            };
+
+            int wait = 13;
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KopylskikhVM.Sprint4.Task5.V29/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.KopylskikhVM.Sprint4.Task5.V29.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Копыльских В. М. | НТм-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Копыльских Виктория Максимовна | НТм-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный массив 5x5, заполненный случайными числами                *");
+        Console.WriteLine("* в диапазоне от -9 до 9. Найти количество положительных элементов.       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] array = new int[5, 5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                array[i, j] = rnd.Next(-9, 10);
+            }
+        }
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Количество положительных элементов = " + ds.Calculate(array));
+
+        Console.ReadLine();
+    }
+}
